d5e2aa046ef568d607ce2540be9fa0e9d87374de
anchenayres <21100284@virtualwindow.co.za>
2023-09-07 01:47:38 +0200

    Backend Implemented
    Backend functionality working, I need to updated the language to correlate to tester project

--- TranslationPage.xaml.cs
+++ TranslationPage.xaml.cs
@@ -1,9 +1,35 @@
-namespace AIService;
+using System;
+using Microsoft.Maui.Controls;
 
-public partial class TranslationPage : ContentPage
+namespace AIService
 {
-	public TranslationPage()
-	{
-		InitializeComponent();
-	}
+    public partial class TranslationPage : ContentPage
+    {
+        private TranslationService translationService = new TranslationService(); // Create an instance of your TranslationService
+
+        public TranslationPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void TranslateButton_Clicked(object sender, EventArgs e)
+        {
+            string textToTranslate = textToTranslateEditor.Text;
+            string sourceLanguage = sourceLanguagePicker.SelectedItem as string;
+            string targetLanguage = targetLanguagePicker.SelectedItem as string;
+
+            if (!string.IsNullOrWhiteSpace(textToTranslate) && !string.IsNullOrWhiteSpace(sourceLanguage) && !string.IsNullOrWhiteSpace(targetLanguage))
+            {
+                // Call your TranslationService to get the translation
+                string translation = await translationService.TranslateAsync(textToTranslate, sourceLanguage, targetLanguage);
+
+                // Update the translationResultLabel with the translation result
+                translationResultLabel.Text = translation;
+            }
+            else
+            {
+                translationResultLabel.Text = "Please fill in all fields.";
+            }
+        }
+    }
 }
--- TranslationService.cs
+++ TranslationService.cs
@@ -1,10 +1,20 @@
 ﻿using System;
 using System.Net.Http;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace AIService
 {
+    public class MyMemoryApiResponse
+    {
+        public ResponseData responseData { get; set; }
+    }
+
+    public class ResponseData
+    {
+        public string translatedText { get; set; }
+    }
     public class TranslationService
     {
         private readonly HttpClient _httpClient;
@@ -17,22 +27,60 @@ namespace AIService
 
         public async Task<string> TranslateAsync(string textToTranslate, string sourceLanguage, string targetLanguage)
         {
-            var requestContent = new StringContent($"q={textToTranslate}&langpair={sourceLanguage}|{targetLanguage}", Encoding.UTF8, "application/x-www-form-urlencoded");
-
-            var response = await _httpClient.PostAsync(MyMemoryApiUrl, requestContent);
-
-            if (response.IsSuccessStatusCode)
+            try
             {
-                var responseBody = await response.Content.ReadAsStringAsync();
-                // Parse the JSON response and extract the translated text
-                // Return the translation
-            }
-            else
-            {
-                // Handle error response
-            }
+                // Ensure textToTranslate is URL-encoded to handle special characters
+                textToTranslate = System.Net.WebUtility.UrlEncode(textToTranslate);
 
-            return null; // Handle errors and return appropriate values
+                // Build the API request URL for translation
+                string apiUrl = $"{MyMemoryApiUrl}?q={textToTranslate}&langpair={sourceLanguage}|{targetLanguage}";
+
+                // Send a GET request to the API
+                var response = await _httpClient.GetAsync(apiUrl);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    try
+                    {
+                        var responseBody = await response.Content.ReadAsStringAsync();
+                        var jsonData = JsonSerializer.Deserialize<MyMemoryApiResponse>(responseBody);
+
+                        if (jsonData != null && jsonData.responseData != null)
+                        {
+                            string translation = jsonData.responseData.translatedText;
+                            return translation;
+                        }
+                        else
+                        {
+                            return "Translation not available.";
+                        }
+                    }
+                    catch (JsonException)
+                    {
+                        return "Failed to parse the translation response.";
+                    }
+                }
+                else
+                {
+                    // Handle non-successful HTTP status codes (e.g., 404, 500)
+                    // You can return appropriate error messages or handle specific cases here
+                    return "Translation request failed with HTTP status code: " + response.StatusCode;
+                }
+            }
+            catch (HttpRequestException ex)
+            {
+                // Handle HTTP request-related exceptions
+                // Log the exception details for troubleshooting
+                // Example: Console.WriteLine("HTTP request failed: " + ex.Message);
+                return "Translation request failed: " + ex.Message;
+            }
+            catch (Exception ex)
+            {
+                // Handle other exceptions (e.g., network issues, timeouts)
+                // Log the exception details for troubleshooting
+                // Example: Console.WriteLine("An error occurred: " + ex.Message);
+                return "An error occurred: " + ex.Message;
+            }
         }
     }
 }
